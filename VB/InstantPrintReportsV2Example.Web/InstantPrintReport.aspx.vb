afcbdbe8f36f05dcabd89c6d6c8e574fbb993cc7
DevExpressExampleBot <fakeEmail>
2020-04-21 14:39:59 +0300

    Source auto update [skip ci]

--- VB/InstantPrintReportsV2Example.Web/InstantPrintReport.aspx.vb
+++ VB/InstantPrintReportsV2Example.Web/InstantPrintReport.aspx.vb
@@ -31,7 +31,7 @@ Namespace InstantPrintReportsV2Example.Web
                         ms.Seek(0, SeekOrigin.Begin)
                         Dim reportContent() As Byte = ms.ToArray()
                         Response.ContentType = "application/pdf"
-						Response.AddHeader("Content-Disposition", "attachement; filename=MyFileName.pdf")
+						Response.AddHeader("Content-Disposition", "attachment; filename=MyFileName.pdf")
                         Response.Clear()
                         Response.OutputStream.Write(reportContent, 0, reportContent.Length)
                         Response.End()
