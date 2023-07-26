3099c939459781fe7c658d9be4e0290c06396397
Dmitry Minakov <dmitry.minakov@devexpress.com>
2023-07-26 20:45:25 +0400

    Revert "23.1 update"

--- CS/EFCore/InstantReportEF/InstantReportEF.Win/Controllers/WinInstantPrintReportController.cs
+++ CS/EFCore/InstantReportEF/InstantReportEF.Win/Controllers/WinInstantPrintReportController.cs
@@ -1,18 +1,21 @@
-﻿using DevExpress.ExpressApp.ReportsV2;
-using DevExpress.XtraReports.UI;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using InstantPrintReportsV2Example.Module.Controllers;
-using Microsoft.Extensions.DependencyInjection;
+using DevExpress.ExpressApp.ReportsV2;
+using DevExpress.XtraReports.UI;
 
 namespace InstantPrintReportsV2Example.Module.Win {
     public class WinInstantPrintReportController : PrintContactsController {
-        public WinInstantPrintReportController() : base() { }
-
-        [ActivatorUtilitiesConstructor]
-        public WinInstantPrintReportController(IServiceProvider serviceProvider) : base(serviceProvider) { }
-
         protected override void PrintReport(IReportDataV2 reportData) {
-            using var report = LoadReport(reportData);
-            report.PrintDialog();
+            var reportStorage = ReportDataProvider.GetReportStorage(Application.ServiceProvider);
+            XtraReport report = reportStorage.LoadReport(reportData);
+            ReportsModuleV2 reportsModule = ReportsModuleV2.FindReportsModule(Application.Modules);
+            if (reportsModule != null && reportsModule.ReportsDataSourceHelper != null) {
+                reportsModule.ReportsDataSourceHelper.SetupBeforePrint(report);
+                report.PrintDialog();
+            }
         }
     }
 }
--- CS/XPO/InstantReport/InstantReport.Blazor.Server/Controllers/BlazorPrintContactsController.cs
+++ CS/XPO/InstantReport/InstantReport.Blazor.Server/Controllers/BlazorPrintContactsController.cs
@@ -1,19 +1,21 @@
 ﻿using DevExpress.ExpressApp.ReportsV2;
 using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.UI;
 using InstantPrintReportsV2Example.Module.Controllers;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace InstantReport.Blazor.Server.Controllers {
     public class BlazorPrintContactsController : PrintContactsController {
-        public BlazorPrintContactsController() : base() { }
-
-        [ActivatorUtilitiesConstructor]
-        public BlazorPrintContactsController(IServiceProvider serviceProvider) : base(serviceProvider) { }
-
         protected override void PrintReport(IReportDataV2 reportData) {
-            using var report = LoadReport(reportData);
-            report.CreateDocument();
-            PrintToolBase tool = new PrintToolBase(report.PrintingSystem);
-            tool.Print();
+            ReportsModuleV2 reportsModule = ReportsModuleV2.FindReportsModule(Application.Modules);
+            if (reportsModule != null && reportsModule.ReportsDataSourceHelper != null) {
+                var reportStorage = ReportDataProvider.GetReportStorage(Application.ServiceProvider);
+                XtraReport report = reportStorage.LoadReport(reportData);
+                reportsModule.ReportsDataSourceHelper.SetupBeforePrint(report);
+                report.CreateDocument();
+                PrintToolBase tool = new PrintToolBase(report.PrintingSystem);
+                tool.Print();
+            }
         }
     }
 }
